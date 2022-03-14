2a7915abcf8fd2fbdc9e2d59be1564fd00a76a35
vj2001 <vishaljain63957@gmail.com>
2022-03-14 13:04:10 +0530

    added work route

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/about",(req,res)=>{
     res.send("<h1><b>You are on the about page. Say Hi !!!</b></h1>")
 })
 
+app.get("/work",(req,res)=>{
+    res.send("<h1><b>You are on the work page. You can work now !!!</b></h1>")
+})
+
 // process.env.PORT
 app.listen(3000,console.log("running on port 3000"));
 
